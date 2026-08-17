6f218590fa51c6e7aaedd5454eb088ca75237803
Anderson Fontes Fernandes Júnior <andersonfontes795@gmail.com>
2026-08-17 19:27:15 -0300

    Add files via upload

---
+++ LaboratorioSeguranca_CorrigidoMarvadamente - 12-08-26/backend/src/controllers/comentarioController_CORRIGIDOMARVADAMENTE.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from "express";
+import db from "../database";
+
+
+export const criarComentario = async (
+    req: Request,
+    res: Response
+) => {
+
+    const {
+        texto,
+        usuarioId
+    } = req.body;
+
+    // const query =
+    //     `INSERT INTO comentario (texto, usuario_id)
+    //      VALUES ('${texto}', '${usuarioId}')`;
+    const query =
+        `INSERT INTO comentario (texto, usuario_id)
+         VALUES ($1, $2)`;
+
+    console.log(`Query Executada: ${query}`);
+
+
+    try {
+
+        // await db.query(query);
+        await db.query(query, [texto, usuarioId]);
+
+        res.status(201).json({
+            message: "Comentário criado"
+        });
+
+    } catch (err: any) {
+
+        res.status(500).json({
+            error: err.message
+        });
+    }
+};
+
+
+export const listarComentarios = async (
+    _req: Request,
+    res: Response
+) => {
+
+    try {
+
+        const result = await db.query(
+            "SELECT * FROM comentario"
+        );
+
+        res.json(result.rows);
+
+    } catch (err: any) {
+
+        res.status(500).json({
+            error: err.message
+        });
+    }
+};
